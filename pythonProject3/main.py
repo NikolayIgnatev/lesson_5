d6663b62923a34ac76f24a4692751dd40961200a
NikolayIgnatev <114672912+NikolayIgnatev@users.noreply.github.com>
2024-11-21 16:58:13 +0300

    code added

---
+++ pythonProject3/main.py
@@ -0,0 +1,73 @@
+import random
+import os
+import file_operations
+from faker import Faker
+
+
+def get_cards(skills,letters,folder_name):
+    for number in range(10):
+        replaced_skills = []
+        selected_skills = random.sample(skills,3)
+        for skill in selected_skills:
+            for letter in skill:
+                skill = skill.replace(letter,letters[letter])
+            replaced_skills.append(skill)
+        skill_one = replaced_skills[0]
+        skill_two = replaced_skills[1]
+        skill_three = replaced_skills[2]
+
+        fake = Faker('ru_RU')
+        (fake.first_name())
+        context = {
+          "first_name":(fake.first_name()),
+          'last_name':(fake.last_name()),
+          'job':(fake.job()),
+          'town':(fake.city()),
+          'strength':random.randint(3,18),
+          'agility':random.randint(3,18),
+          'endurance':random.randint(3,18),
+          'intelligence':random.randint(3,18),
+          'luck':random.randint(3,18),
+          'skill_1':skill_one,
+          'skill_2':skill_two,
+          'skill_3':skill_three
+      }
+
+
+        file_operations.render_template("card.svg", f"{folder_name}/result_{number}.svg", context)
+
+def main():
+    skills = ['Стремительный прыжок', 'Электрический выстрел', 'Ледяной удар', 'Стремительный удар', 'Кислотный взгляд'
+        , 'Тайный побег', 'Ледяной выстрел', 'Огненный заряд']
+    letters = {
+        'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+        'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+        'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+        'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+        'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+        'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+        'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+        'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+        'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+        'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+        'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+        'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+        'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+        'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+        'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+        'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+        'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+        'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+        'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+        'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+        'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+        'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+        ' ': ' '
+    }
+    folder_name = 'cards'
+    if not os.path.isdir(folder_name):
+        os.mkdir(folder_name)
+    get_cards(skills, letters, folder_name)
+
+if __name__ == '__main__':
+    main()
